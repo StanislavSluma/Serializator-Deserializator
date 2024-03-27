6a2435662d578be134c0da0be2f2f81df246abbc
Sluma <bekarevstanislav@gmail.com>
2024-03-27 19:23:41 +0300

    Initialize commit

---
+++ Serializator&&Deserializator/Serializator&&Deserializator/RequestSerializer.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace Serializator__Deserializator
+{
+    public class RequestSerializer
+    {
+        public static string Serialize(string tag, params object[] objs)
+        {
+            string res = tag;
+            if (res[^1] != '?')
+                res += '?';
+            foreach (object obj in objs)
+            {
+                res += JsonSerializer.Serialize(obj) + "!";
+            }
+            return res;
+        }
+
+        public static List<string> Deserializer(string qwerty)
+        {
+            string tag = qwerty.Substring(0, qwerty.IndexOf('?') + 1);
+            qwerty = qwerty.Remove(0, qwerty.IndexOf('?') + 1);
+            List<string> res = new List<string>() { tag };
+            string arg = "";
+            bool IsString = false;
+            for (int i = 0; i < qwerty.Length; i++)
+            {
+                if (qwerty[i] == '\"')
+                {
+                    IsString = !IsString;
+                    continue;
+                }
+                if (qwerty[i] == '!' && !IsString)
+                {
+                    res.Add(arg);
+                    arg = "";
+                    continue;
+                }
+                arg += qwerty[i];
+            }
+            return res;
+        }
+    }
+}
---
+++ Serializator&&Deserializator/Serializator&&Deserializator/Serializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Serializator__Deserializator
+{
+    public class Serializer
+    {
+        public static string SerializeJson(object obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+
+        public static T? DeserializeJson<T>(string obj)
+        {
+            return JsonSerializer.Deserialize<T>(obj);
+        }
+    }
+}
